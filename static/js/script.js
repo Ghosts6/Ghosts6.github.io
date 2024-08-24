d2810af7224c4bf48abd528baa6edfa75477847c
Kiarash <95994481+Ghosts6@users.noreply.github.com>
2024-08-24 10:07:52 +0100

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -51,48 +51,6 @@ window.addEventListener('load', function() {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    const imgElement = document.getElementById("meme-image");
-
-    function generateMemeUrls(count) {
-        const memes = [];
-        for (let i = 1; i <= count; i++) {
-            memes.push(`static/img/meme/meme${i}.jpg`);
-        }
-        return memes;
-    }
-
-    const memeCount = 10; 
-    const memes = generateMemeUrls(memeCount);
-
-    const fallbackImageUrl = "static/img/500.jpg";
-
-    function getRandomMeme() {
-        const randomIndex = Math.floor(Math.random() * memes.length);
-        return memes[randomIndex];
-    }
-
-    function displayRandomMeme() {
-        const randomMemeUrl = getRandomMeme();
-
-        const tempImg = new Image();
-        tempImg.src = randomMemeUrl;
-
-        tempImg.onload = function() {
-            imgElement.src = randomMemeUrl;
-        };
-
-        tempImg.onerror = function() {
-            imgElement.src = fallbackImageUrl;
-        };
-
-        imgElement.style.height = "400px";
-        imgElement.style.display = "block";
-        imgElement.style.margin = "0 auto";
-        imgElement.alt = "Random Dev Meme";
-    }
-
-    displayRandomMeme();
-
     const scrollToTopButton = document.getElementById('scrollToTopButton');
     window.addEventListener('scroll', () => {
         scrollToTopButton.style.display = window.scrollY > 100 ? 'block' : 'none';
