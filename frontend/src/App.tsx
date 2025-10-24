8864b9d294fec2fef9c1f17868a818f42474de71
ghosts6 <kiarash82.42@gmail.com>
2025-10-24 14:06:52 -0400

    fix for githubpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Layout from './components/common/Layout';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
